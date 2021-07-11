3b97482b90f777e2af89ee7acd33c7c80e9bc900
Айрат Мухаметшин <a.mukhametshin@bars.group>
2021-07-12 00:12:43 +0300

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './../App.css';
+
+const Header = ()=> {
+    return (
+        <header className="header">
+            <img src="https://im0-tub-ru.yandex.net/i?id=96eb68d523468f3e30305601ed5d5276&n=13&exp=1"/>
+        </header>
+    )
+};
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './../App.css';
+
+const Profile = ()=> {
+    return (
+        <div className="content">
+            <img src="https://im0-tub-ru.yandex.net/i?id=96eb68d523468f3e30305601ed5d5276&n=13&exp=1"/>
+        </div>
+    )
+};
+
+export default Profile;
