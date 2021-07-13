ce1e5f6e1eda379897db8e2597c82ff83083fa9d
Айрат Мухаметшин <a.mukhametshin@bars.group>
2021-07-13 23:46:40 +0300

    added Dialogs

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css'
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 const App = () => {
     return (
         <div className="app-wrapper">
             <Header/>
             <Sidebar/>
-            <Profile/>
+            {/*<Profile/>*/}
+            <div className="content">
+                <Dialogs/>
+                {/*<Profile/>*/}
+            </div>
         </div>
     );
 }
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,7 +4,7 @@ import PostsList from "./Posts/PostsList";
 
 const Profile = ()=> {
     return (
-        <div className="content">
+        <div>
             <img src="https://im0-tub-ru.yandex.net/i?id=96eb68d523468f3e30305601ed5d5276&n=13&exp=1"/>
             <PostsList/>
         </div>
